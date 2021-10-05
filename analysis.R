f69d9f1c96cc16ab9b7516863917a7c105d0d39a
andreasbueckle <abueckle@indiana.edu>
2021-10-05 15:26:55 -0400

    Updated x-label in bar graph

--- analysis.R
+++ analysis.R
@@ -8,8 +8,8 @@ remove_webpage = with_counts[with_counts$event_name != 'webpage', ]
 
 ggplot(remove_webpage, aes(x=event_name, y=n, fill=hostname))+
   geom_bar(stat='identity', position = 'dodge')+
-  xlab('User-generated event')+
-  ylab('Number of loggedn events')+
+  xlab('User-generated events (until 10/5/21)')+
+  ylab('Number of logged events')+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=15))+
   theme(axis.text.y = element_text(size=15))+
   theme(legend.position="bottom" )+ 
